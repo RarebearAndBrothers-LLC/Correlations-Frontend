a3fad41b895c6f82af14df3192b4d27fd659e19a
Ryan Ghaida <ryaboug@gmail.com>
2019-04-27 19:16:00 -0400

    just amke login

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-         <h1>Correlations</h1>
+         <h1>Just make a login</h1>
          <h3>An app to track and correrlate habits; how much does doing X effect Y ? What are the most effective habits to facilitate systematic changes to the individual? </h3> 
         </header>
       </div>
